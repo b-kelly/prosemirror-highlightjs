7267817ba8d79ab900acd5436ac57c93e5b93de6
Ben Kelly <b-kelly@users.noreply.github.com>
2020-07-21 14:56:01 -0400

    update what gets added to the package

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
-    libraryTarget: "commonjs",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+    mode: "production",
+    entry: {
+        index: "./src/index.ts",
+        "sample-schema": "./src/sample-schema.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        libraryTarget: "commonjs",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    externals: [
+        "prosemirror-model",
+        "prosemirror-state",
+        "prosemirror-view",
+        "highlight.js",
     ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  externals: [
-    "prosemirror-model",
-    "prosemirror-state",
-    "prosemirror-view",
-    "highlight.js",
-  ],
 };
