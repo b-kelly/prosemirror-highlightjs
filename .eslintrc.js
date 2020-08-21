0196c6082906ccaca9cf952f0f42d31308cec1b8
Ben Kelly <b-kelly@users.noreply.github.com>
2020-08-20 23:12:25 -0400

    add eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.eslint.json"],
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    plugins: ["@typescript-eslint", "jest"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+    ],
+    rules: {
+        "no-console": "error",
+        "no-alert": "error",
+    },
+};
